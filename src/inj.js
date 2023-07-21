8646a58989625cc18ab9c53969cd4b5667df44ef
Mike Clark <mclarkgb@gmail.com>
2023-07-21 22:10:22 +0100

    updating to allow textarea inputs

--- src/inj.js
+++ src/inj.js
@@ -1,59 +1,72 @@
-getStyle = function(element, name)
+const getStyle = function(element, name)
 {
-  return element.currentStyle ? element.currentStyle[name] : window.getComputedStyle ? window.getComputedStyle(element, null).getPropertyValue(name) : null;
-};
+  return element.currentStyle ? element.currentStyle[name] : window.getComputedStyle ? window.getComputedStyle(element, null).getPropertyValue(name) : null
+}
 
-focusOnFirstInput = function () {
-  if (document.activeElement.tagName != "INPUT") {
-    const inputs = document.body.getElementsByTagName("input");
-    let focused = false;
+const focusOnFirstInput = function () {
+  const validTagNames = ['INPUT', 'TEXTAREA']
+  let isFocusedOnValidInput = !!validTagNames.find((tagName) => tagName === document.activeElement.tagName)
+  
+  if (!isFocusedOnValidInput) {
+    const textInputs = document.body.querySelectorAll("input,textarea")
+    let focused = false
 
-    for (let i = 0; i < inputs.length; i++) {
-      const input = inputs[i];
-      const isHiddenByDisplay = getStyle(input, 'display') === 'none';
-      const isHiddenByVisibility = getStyle(input, 'visibility') === 'hidden';
-      const isHidden = isHiddenByDisplay || isHiddenByVisibility;
-      const validInputTypes = ['text', 'search', 'email', 'number', 'password', 'tel', 'url'];
-
-      const isValidFocusableField = validInputTypes.find(x => x === input.type);
-      const isDisabledOrReadonly = input.disabled || input.readOnly;
+    for (let i = 0; i < textInputs.length; i++) {
+      const textInput = textInputs[i]
+      const isHiddenByDisplay = getStyle(textInput, 'display') === 'none'
+      const isHiddenByVisibility = getStyle(textInput, 'visibility') === 'hidden'
+      const isHidden = isHiddenByDisplay || isHiddenByVisibility
+      const isDisabledOrReadonly = textInput.disabled || textInput.readOnly
       
-      if (!isHidden && !isDisabledOrReadonly && elementInViewport(input) && isValidFocusableField) {
-        input.focus();
-        focused = true;
-        break;
+      let isValidFocusableField = false
+      switch (textInput.tagName) {
+        case 'INPUT': {
+          const validInputTypes = ['text', 'search', 'email', 'number', 'password', 'tel', 'url']
+          isValidFocusableField = !!validInputTypes.find(validInputType => validInputType === textInput.type)
+          break
+        }
+        case 'TEXTAREA': 
+          isValidFocusableField = true
+          break
+      }
+      
+      if (!isHidden && !isDisabledOrReadonly && elementInViewport(textInput) && isValidFocusableField) {
+        textInput.focus()
+        focused = true
+        break
       }
     }
 
-    if (!focused && inputs.length > 0)
-      for (let i = 0; i < inputs.length; i++) {
-        const input = inputs[i];
-        if (elementInViewport(input)) {
-          input.focus();
-          focused = true;
-          break;
+    if (!focused && textInputs.length > 0)
+      for (let i = 0; i < textInputs.length; i++) {
+        const textInput = textInputs[i]
+        if (elementInViewport(textInput)) {
+          textInput.focus()
+          focused = true
+          break
         }
       }
   }
-};
+}
 
-elementInViewport = function (el) {
-  var bounding = el.getBoundingClientRect();
+const elementInViewport = function (el) {
+  var bounding = el.getBoundingClientRect()
   const isInViewport = (
     bounding.top >= 0 &&
     bounding.left >= 0 &&
     bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
     bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+  )
 
-  return isInViewport;
-};
+  return isInViewport
+}
 
+// eslint-disable-next-line no-undef
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.action == "focusOnFirstInput") {
-      focusOnFirstInput();      
-      sendResponse({complete: true});
+      focusOnFirstInput()      
+      sendResponse({complete: true})
     }
   }
-);
+)
